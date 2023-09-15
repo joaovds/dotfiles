609f5f6fe034529244978b5ea0f6ec872042c058
João Victor da Silva <jv782063@gmail.com>
2023-09-15 07:44:07 -0300

    update kitty and lunarvim config file

--- lunarvim/config.lua
+++ lunarvim/config.lua
@@ -1,24 +1,27 @@
 -- Read the docs: https://www.lunarvim.org/docs/configuration
+-- Example configs: https://github.com/LunarVim/starter.lvim
 -- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
 -- Forum: https://www.reddit.com/r/lunarvim/
 -- Discord: https://discord.com/invite/Xb9B4Ny
+
+lvim.transparent_window = true
 
 lvim.plugins = {
   {
     "rmagatti/goto-preview",
     config = function()
-    require('goto-preview').setup {
-          width = 120; -- Width of the floating window
-          height = 25; -- Height of the floating window
-          default_mappings = false; -- Bind default mappings
-          debug = false; -- Print debug information
-          opacity = nil; -- 0-100 opacity level of the floating window where 100 is fully transparent.
-          post_open_hook = nil -- A function taking two arguments, a buffer and a window to be ran as a hook.
-          -- You can use "default_mappings = true" setup option
-          -- Or explicitly set keybindings
-          -- vim.cmd("nnoremap gpd <cmd>lua require('goto-preview').goto_preview_definition()<CR>")
-          -- vim.cmd("nnoremap gpi <cmd>lua require('goto-preview').goto_preview_implementation()<CR>")
-          -- vim.cmd("nnoremap gP <cmd>lua require('goto-preview').close_all_win()<CR>")
+      require('goto-preview').setup {
+        width = 120; -- Width of the floating window
+        height = 25; -- Height of the floating window
+        default_mappings = false; -- Bind default mappings
+        debug = false; -- Print debug information
+        opacity = nil; -- 0-100 opacity level of the floating window where 100 is fully transparent.
+        post_open_hook = nil -- A function taking two arguments, a buffer and a window to be ran as a hook.
+        -- You can use "default_mappings = true" setup option
+        -- Or explicitly set keybindings
+        -- vim.cmd("nnoremap gpd <cmd>lua require('goto-preview').goto_preview_definition()<CR>")
+        -- vim.cmd("nnoremap gpi <cmd>lua require('goto-preview').goto_preview_implementation()<CR>")
+        -- vim.cmd("nnoremap gP <cmd>lua require('goto-preview').close_all_win()<CR>")
       }
     end
   },
